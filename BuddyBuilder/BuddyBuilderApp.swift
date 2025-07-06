735b7565a95ff850b02316852cc5bbf3023ed4a9
OrxanMusayev <orxanmusayev1102@gmail.com>
2025-07-06 15:45:25 +0400

    Initial Commit

---
+++ BuddyBuilder/BuddyBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuddyBuilderApp.swift
+//  BuddyBuilder
+//
+//  Created by Orxan Musayev on 06.07.25.
+//
+
+import SwiftUI
+
+@main
+struct BuddyBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
